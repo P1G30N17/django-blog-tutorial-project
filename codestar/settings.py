08ab7eb502448ad3f6fd5915c0ebad1aa560f6ee
James King <jamesking.1711@gmail.com>
2024-09-02 09:28:18 +0000

    Deployment issues.

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bo)tlt972&^)_y$2bblr3&2gduw$0+8tg5)5upm1f3yh-x!^ph'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
