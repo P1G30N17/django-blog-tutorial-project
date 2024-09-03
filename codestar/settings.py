94d3d914e8aceaec479eac736f23a544a00585c6
James King <jamesking.1711@gmail.com>
2024-09-03 11:00:35 +0000

    Template inheritance.

--- codestar/settings.py
+++ codestar/settings.py
@@ -63,7 +63,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [TEMPLATES_DIR],
-        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
